b346b044e6c97f4b27e7e3ca0ebce47ae0ef6bd6
O Teerasak Vichadee <iolumin@gmail.com>
2021-11-07 18:31:38 +0700

    add simple solana contract

--- epic-project/programs/epic-project/src/lib.rs
+++ epic-project/programs/epic-project/src/lib.rs
@@ -1,14 +1,14 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BS4U4Dj4fr1UQQ15Ehu9sADe9jRjZ3c4doAWiH2qFq64");
 
 #[program]
-pub mod epic_project {
-    use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
-    }
+pub mod epicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
