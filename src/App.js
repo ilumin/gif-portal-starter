e4c315224c79443a026a4dce27df275021efa1af
O Teerasak Vichadee <iolumin@gmail.com>
2021-11-09 09:16:01 +0700

    clear input

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const TEST_GIFS = [
   "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
-  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif",
+  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif",
   "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
 ];
 
@@ -145,6 +145,8 @@ const App = () => {
           },
         });
         console.log("GIF successfully sent!");
+
+        setInputValue("");
 
         await getGifList();
       } catch (error) {
